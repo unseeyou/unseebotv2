2e1fddff0dfa0d88dff04fa4e7e65e0497a83e18
unseeyou <siqi.chen111@gmail.com>
2024-05-23 20:41:26 +1000

    added fly me to the moon as a music command

--- cogs/music.py
+++ cogs/music.py
@@ -100,6 +100,28 @@ class Music(commands.Cog):
         else:
             await interaction.followup.send(f'Error: {output}')
 
+    @app_commands.command(description='and let me play among the stars')
+    async def fly_me_to_the_moon(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True, thinking=True)
+        channel = interaction.user.voice.channel
+        voice = await channel.connect(self_deaf=True)
+        try:
+            output = await play_fly_me_to_the_moon(voice)
+        except Exception as err:
+            output = str(err)
+
+        if output == 0:
+            await interaction.followup.send(f'{interaction.user.name} is having NGE PTSD')
+            while voice is not None:
+                if voice.is_playing():
+                    await asyncio.sleep(0.2)
+                if not voice.is_connected():
+                    break
+                if not voice.is_playing():
+                    await play_fly_me_to_the_moon(voice)
+        else:
+            await interaction.followup.send(f'Error: {output}')
+
     @commands.hybrid_command(help='leaves the vc and stops playing audio')
     async def stop(self, ctx):
         """Stops and disconnects the bot from voice"""
