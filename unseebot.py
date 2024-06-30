ec1e838684f3e1d6db39a15d3113159732057fb8
unseeyou <siqi.chen111@gmail.com>
2024-06-30 19:19:40 +1000

    formatting

--- unseebot.py
+++ unseebot.py
@@ -13,7 +13,14 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', help_command=None, case_insensitive=True, intents=intents, activity=discord.Game('With your mind - >help'), status=discord.Status.online)
+bot = commands.Bot(
+    command_prefix='>',
+    help_command=None,
+    case_insensitive=True,
+    intents=intents,
+    activity=discord.Game('With your mind - >help'),
+    status=discord.Status.online
+)
 
 load_dotenv()
 TOKEN = os.getenv("UNSEEBOT_TOKEN")
